673cae88a4524df865f31021aef5b9a6895829f9
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-13 15:26:20 +0300

    finish notes buttom sheet

--- lib/view/widgets/add_note_buttom_sheet.dart
+++ lib/view/widgets/add_note_buttom_sheet.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:notesapp/constant.dart';
+import 'package:notesapp/view/widgets/custom_buttom.dart';
 import 'package:notesapp/view/widgets/custom_text_field.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -9,15 +11,18 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: const [
-          SizedBox(height: 32,),
-          CustomTextField(hintname: 'Title',maxline: 2,),
-          SizedBox(height: 16,),
-          CustomTextField(hintname: 'Content',maxline: 5,),
-
-        ],
-      
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(height: 32,),
+            CustomTextField(hintname: 'Title',maxline: 2,),
+            SizedBox(height: 16,),
+            CustomTextField(hintname: 'Content',maxline: 5,),
+            SizedBox(height: 32,),
+            CustomButtom(),
+          ],
+        
+        ),
       ),
     );
   }
---
+++ lib/view/widgets/custom_buttom.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:notesapp/constant.dart';
+
+class CustomButtom extends StatelessWidget {
+  const CustomButtom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55 ,
+      decoration: BoxDecoration(
+        color: KPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(child: Text('Add' , style: TextStyle(color: Colors.black ,fontSize: 20,fontWeight: FontWeight.bold),)),
+    );
+  }
+}
