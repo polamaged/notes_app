d4bf30159621fc3b8b4220b3cd69fdbb31f11a2e
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-13 21:23:22 +0300

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notesapp/Model/note_model.dart';
 import 'package:notesapp/constant.dart';
 import 'package:notesapp/view/notes_view.dart';
 
@@ -8,6 +9,7 @@ void main() async{
   await Hive.initFlutter();
 
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const notesapp());
 }
 
