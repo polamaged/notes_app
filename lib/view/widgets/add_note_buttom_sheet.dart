766fddcef85c53455abc0177e895684b2396bc47
polamaged <74374673+polamaged@users.noreply.github.com>
2025-06-14 19:37:15 +0300

    refactor multi bloc provider

--- lib/view/widgets/add_note_buttom_sheet.dart
+++ lib/view/widgets/add_note_buttom_sheet.dart
@@ -2,29 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notesapp/view/widgets/add_note_form.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if(state is AddNoteFailure)
-          {
-            print('failed ${state.errMessage}');
-          }
-          if(state is AddNoteSuccess)
-          {
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          return const SingleChildScrollView(child: AddNoteForm());
-        }, 
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print('failed ${state.errMessage}');
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return const SingleChildScrollView(child: AddNoteForm());
+          },
+        ),
       ),
     );
   }
